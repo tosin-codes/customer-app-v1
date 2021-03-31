12202b43f658b9f541283e8d80c71f05aa76d533
Tosin Ezekiel <seunezekiel11@gmail.com>
2021-03-31 23:13:20 +0100

    fix

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="grid grid-cols-12 mx-auto">
-    <!-- <GeneralNav /> -->
-      <div class="mt-5">
-        <div class="flex flex-row items-center mb-10">
-            <div>
-              <img class="w-8 mr-4" src="~/assets/svg/dashboard.svg" alt="" />
-            </div>
-            <div class="font-bold text-gray-700">Dashboard</div>
-        </div>
-        <!-- <DashboardCards /> -->
-        <!-- <div v-if="!this.$store.getters.active_loans">
-          <ActiveLoan />
-        </div> -->
-        <!-- <div v-else>
-          <ActiveLoanSchedule />
-        </div> -->
-      </div>
+  <div>
+    <div class="grid grid-cols-12 mx-auto">
+      <!-- <GeneralNav /> -->
+        <!-- <div class="mt-5"> -->
+          <!-- <div class="flex flex-row items-center mb-10">
+              <div>
+                <img class="w-8 mr-4" src="~/assets/svg/dashboard.svg" alt="" />
+              </div>
+              <div class="font-bold text-gray-700">Dashboard</div>
+          </div> -->
+          <!-- <DashboardCards /> -->
+          <!-- <div v-if="!this.$store.getters.active_loans">
+            <ActiveLoan />
+          </div> -->
+          <!-- <div v-else>
+            <ActiveLoanSchedule />
+          </div> -->
+        <!-- </div> -->
+    </div>
   </div>
 </template>
 
