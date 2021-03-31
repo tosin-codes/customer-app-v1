b628da50b0eac308ccbe127559e7138dff91b12c
Tosin Ezekiel <seunezekiel11@gmail.com>
2021-03-31 22:06:35 +0100

    fix

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,36 +1,35 @@
 <template>
-<div>
-  <h1>Hello World!</h1>
-</div>
-  <!-- <div class="grid grid-cols-12 maxWidth mx-auto"> -->
-    <!-- <GeneralNav /> -->
-    <!-- <div class="my-container">
-      <div class="mt-5">
-        <div class="flex flex-row items-center mb-10">
-          <div>
-            <img class="w-8 mr-4" src="~/assets/svg/dashboard.svg" alt="" />
+  <div>
+    <div class="grid grid-cols-12 maxWidth mx-auto">
+      <!-- <GeneralNav /> -->
+      <div class="my-container">
+        <div class="mt-5">
+          <div class="flex flex-row items-center mb-10">
+            <div>
+              <img class="w-8 mr-4" src="~/assets/svg/dashboard.svg" alt="" />
+            </div>
+            <div class="font-bold text-gray-700">Dashboard</div>
           </div>
-          <div class="font-bold text-gray-700">Dashboard</div>
-        </div> -->
-        <!-- <DashboardCards /> -->
-        <!-- <div v-if="!this.$store.getters.active_loans">
-          <ActiveLoan />
+          <!-- <DashboardCards /> -->
+          <!-- <div v-if="!this.$store.getters.active_loans">
+            <ActiveLoan />
+          </div>
+          <div v-else>
+            <ActiveLoanSchedule />
+          </div> -->
         </div>
-        <div v-else>
-          <ActiveLoanSchedule />
-        </div> -->
-      <!-- </div>
+      </div>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script>
-// import GeneralNav from '~/components/GeneralNavbarComponent'
-// import DashboardCards from '~/components/DashboardCards'
+import GeneralNav from '~/components/GeneralNavbarComponent'
+import DashboardCards from '~/components/DashboardCards'
 export default {
   components: {
-    // DashboardCards,
-    // GeneralNav,
+    DashboardCards,
+    GeneralNav,
   },
   data() {
     return {
