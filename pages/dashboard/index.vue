fefa794ad395eb8d525e8e63dc45e3ae510b0f10
Tosin Ezekiel <seunezekiel11@gmail.com>
2021-04-01 00:55:18 +0100

    fix

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,29 +1,57 @@
 <template>
-  <div>
+  <div class="grid grid-cols-12 mx-auto">
     <client-only>
-      <div class="">
-          <div class="">
-              <h1 class="">
-                Dashboard
-              </h1>
+      <GeneralNav />
+        <div class="mt-5">
+          <div class="flex flex-row items-center mb-10">
+            <div>
+              <img class="w-8 mr-4" src="~/assets/svg/dashboard.svg" alt="" />
+            </div>
+            <div class="font-bold text-gray-700">Dashboard</div>
           </div>
-      </div>
-    </client-only>
+          <DashboardCards />
+          <div v-if="!this.$store.getters.active_loans">
+            <ActiveLoan />
+          </div>
+          <div v-else>
+            <ActiveLoanSchedule />
+          </div>
+        </div>
+      </client-only>
   </div>
 </template>
 
 <script>
+// import GeneralNav from '~/components/GeneralNavbarComponent'
+// import DashboardCards from '~/components/DashboardCards'
 export default {
   components: {
+    // DashboardCards,
+    // GeneralNav,
   },
   data() {
     return {
+      // inActiveLoan: false,
+      // activeLoan: false,
     }
   },
+  // middleware: ['auth', 'setLevelForNewLoan'],
   mounted() {},
 }
 </script>
 
 <style>
+.my-container {
+  @apply mt-16 col-start-1 col-end-13 px-3 py-6 mx-auto bg-gray-200 bg-opacity-50 min-h-screen rounded-md z-10
+}
 
+@screen md {
+  .my-container {
+    @apply col-start-4 col-end-13 p-12 w-full
+  }
+}
+
+.maxWidth {
+  max-width: 1500px;
+}
 </style>
