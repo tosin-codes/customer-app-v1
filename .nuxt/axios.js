142ef90753b3d3d04932ce543522c1e9dbf57ccf
Tosin Ezekiel <seunezekiel11@gmail.com>
2021-04-06 01:16:16 +0100

    fix

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -148,8 +148,8 @@ export default (ctx, inject) => {
   const runtimeConfig = ctx.$config && ctx.$config.axios || {}
   // baseURL
   const baseURL = process.browser
-    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'http://127.0.0.1:8000/api/v1/')
-      : (runtimeConfig.baseURL || runtimeConfig.baseUrl || process.env._AXIOS_BASE_URL_ || 'http://127.0.0.1:8000/api/v1/')
+    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'https://cashapi.cashdrive.co/api/v1/')
+      : (runtimeConfig.baseURL || runtimeConfig.baseUrl || process.env._AXIOS_BASE_URL_ || 'https://cashapi.cashdrive.co/api/v1/')
 
   // Create fresh objects for all default header scopes
   // Axios creates only one which is shared across SSR requests!
